938ec3d2a66fb917892408052776150801e18a73
damagodon <nagahashi1710@gmail.com>
2022-02-19 21:44:28 +0900

    製品名の修正

--- ImagefileDelete/Properties/AssemblyInfo.cs
+++ ImagefileDelete/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
 [assembly: AssemblyTitle("ImagefileDelete")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("メモで使用されていない画像ファイルを削除する")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImagefileDelete")]
